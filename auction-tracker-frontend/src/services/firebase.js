892e69aedd37666bb0796d727f8f102c06ecf696
Mohammed Alam <ma3655@rit.edu>
2025-03-19 23:42:02 -0400

    change firebase auth to env vars

--- auction-tracker-frontend/src/services/firebase.js
+++ auction-tracker-frontend/src/services/firebase.js
@@ -5,7 +5,15 @@ import { getFirestore, collection, doc, setDoc, getDoc, updateDoc, deleteDoc, qu
 
 // Your web app's Firebase configuration
 // Replace these values with your own Firebase project config
-
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 
 // Initialize Firebase
